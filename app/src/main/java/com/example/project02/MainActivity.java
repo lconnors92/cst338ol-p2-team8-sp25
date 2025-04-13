d1781d3c286d9bf5f3fb4eecd2de4a0942e4ae9d
Ethan <ethangarcia0927@gmail.com>
2025-04-12 20:44:52 -0700

    Updated build file for viewBinding

--- app/src/main/java/com/example/project02/MainActivity.java
+++ app/src/main/java/com/example/project02/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.project02;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
+
+import com.example.project02.database.entities.User;
+import com.example.project02.database.repositories.UserRepository;
+import com.example.project02.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MAIN_ACTIVITY_USER_ID = "MAIN_ACTIVITY_USER_ID";
+    private static final String SHARED_PREFERENCE_USERID_KEY = "SHARED_PREFERENCE_USERID_KEY";
+    private static final int LOGGED_OUT = -1;
+
+    private ActivityMainBinding binding;
+    private int loggedInUserId = -1;
+    private User user;
 
     public static final String TAG = "TEAM8_P2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+            return ;
+        }
     }
-}
