998073c47d9521ea4d4ea708457025d043ac79bf
Ethan <ethangarcia0927@gmail.com>
2025-04-12 21:36:33 -0700

    Wired up the login section of LoginActivity.java. 2 users are in, testuser1 and admin1. Still need to wire Signup aspect.

--- app/src/main/java/com/example/project02/LoginActivity.java
+++ app/src/main/java/com/example/project02/LoginActivity.java
@@ -1,28 +1,68 @@
 package com.example.project02;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 
+import com.example.project02.database.AppDatabase;
 import com.example.project02.database.entities.User;
+import com.example.project02.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String MAIN_ACTIVITY_USER_ID = "MAIN_ACTIVITY_USER_ID";
-    private static final String SHARED_PREFERENCE_USERID_KEY = "SHARED_PREFERENCE_USERID_KEY";
-    private static final int LOGGED_OUT = -1;
-
-
-    private int loggedInUserId = -1;
-    private User user;
-
-    public static final String TAG = "TEAM8_P2";
+    private ActivityLoginBinding binding;
+    private AppDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-            return ;
-        }
+        database = AppDatabase.getDatabase(this);
+
+        binding.loginButton.setOnClickListener(v -> verifyUser());
+
     }
+
+    private void verifyUser() {
+        String username = binding.usernameEditText.getText().toString().trim();
+        String password = binding.passwordEditText.getText().toString().trim();
+
+        if (username.isEmpty()) {
+            toastMaker("Username Cannot be blank");
+            return;
+        }
+
+        LiveData<User> userObserver = database.userDAO().getUserByUserName(username);
+        userObserver.observe(this,user -> {
+            if (user != null) {
+                if (password.equals(user.getPassword())) {
+                    startActivity(MainActivity.mainActivityIntentFactory(getApplicationContext(),user.getId()));
+                    finish();
+                } else {
+                    toastMaker("Invalid password");
+                    binding.passwordEditText.setSelection(0);
+                }
+            } else {
+                toastMaker(username + " is not a valid username");
+                binding.usernameEditText.setSelection(0);
+            }
+        });
+    }
+
+
+
+    private void toastMaker(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    static Intent loginIntentFactory(Context context) {
+        return new Intent(context, LoginActivity.class);
+    }
+}
