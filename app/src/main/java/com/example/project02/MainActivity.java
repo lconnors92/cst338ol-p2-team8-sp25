52022c36c8020c4d9725092f3a1da52deef7bf6b
Ethan <ethangarcia0927@gmail.com>
2025-04-17 22:16:10 -0700

    Logout Button added to MainActivity.java

--- app/src/main/java/com/example/project02/MainActivity.java
+++ app/src/main/java/com/example/project02/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.project02;
 import static java.lang.String.*;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,23 +24,44 @@ import com.example.project02.database.GameRepository;
 import com.example.project02.database.entities.User;
 import com.example.project02.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private static final String MAIN_ACTIVITY_USER_ID = "MAIN_ACTIVITY_USER_ID";
     public static final String TAG = "APP_DATABASE";
     private ActivityMainBinding binding;
-
-
-    @SuppressLint("DefaultLocale")
+    private static final String SHARED_PREFERENCE_USERID_KEY = "SHARED_PREFERENCE_USERID_KEY";
+    private static final String SAVED_INSTANCE_STATE_USERID_KEY = "SAVED_INSTANCE_STATE_USERID_KEY";
+    private static final int LOGGED_OUT = -1;
+    private int loggedInUserId = -1;
+    private User user;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.logoutToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
+        loginUser(savedInstanceState);
+
+        if (loggedInUserId == LOGGED_OUT) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
+
+
         //Admin Checker
         AppDatabase database = AppDatabase.getDatabase(this);
-        int userId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
-        database.userDAO().getUserByUserId(userId).observe(this, user -> {
+        database.userDAO().getUserByUserId(loggedInUserId).observe(this, user -> {
+
             if (user != null) {
+                this.user = user;
+                invalidateOptionsMenu();
+
                 // Show/hide admin button
                 binding.adminButton.setVisibility(user.isAdmin() ? View.VISIBLE : View.GONE);
                 binding.welcomeTextView.setText(format("Welcome, %s, to Forge & Fate! ", user.getUsername()));
@@ -43,13 +69,14 @@ public class MainActivity extends AppCompatActivity {
                 setupCharacterButtons();
 
                 //Character owned display
-                database.characterDAO().getCharacterCountByUser(userId).observe(this, count -> {
-                    binding.characterCountTextView.setText(String.format("Characters Owned: %d", count));
-                });
+                database.characterDAO().getCharacterCountByUser(loggedInUserId).observe(this, this::onChanged);
+            } else {
+                logout();
             }
         });
 
     }
+
     private void setupCharacterButtons() {
         // Button to create a character
         binding.createCharacterButton.setOnClickListener(v -> {
@@ -74,11 +101,83 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void loginUser(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_USERID_KEY, Context.MODE_PRIVATE);
+        loggedInUserId = sharedPreferences.getInt(SHARED_PREFERENCE_USERID_KEY, LOGGED_OUT);
 
+        if (loggedInUserId == LOGGED_OUT && savedInstanceState != null) {
+            loggedInUserId = savedInstanceState.getInt(SAVED_INSTANCE_STATE_USERID_KEY, LOGGED_OUT);
+        }
+        if (loggedInUserId == LOGGED_OUT) {
+            loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+        } else {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt(SHARED_PREFERENCE_USERID_KEY, loggedInUserId);
+            editor.apply();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.logoutMenuItem);
+        if (user != null) {
+            item.setTitle(user.getUsername());
+            item.setVisible(true);
+        } else {
+            item.setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logoutMenuItem) {
+            showLogoutDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showLogoutDialog() {
+        new AlertDialog.Builder(this).setMessage("Would you like to logout?")
+                .setPositiveButton("Logout", (dialog, which) -> logout())
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                .create().show();
+    }
+
+    private void logout() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_USERID_KEY, Context.MODE_PRIVATE);
+        sharedPreferences.edit().clear().apply();
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SAVED_INSTANCE_STATE_USERID_KEY, loggedInUserId);
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_USERID_KEY,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+        sharedPrefEditor.putInt(MainActivity.SHARED_PREFERENCE_USERID_KEY, loggedInUserId);
+        sharedPrefEditor.apply();
+
+    }
 
     static Intent mainActivityIntentFactory(Context context, int userId) {
         Intent intent = new Intent(context, MainActivity.class);
         intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
         return intent;
+    }
+
+    private void onChanged(Integer count) {
+        binding.characterCountTextView.setText(format("Characters Owned: %d", count));
     }
 }
